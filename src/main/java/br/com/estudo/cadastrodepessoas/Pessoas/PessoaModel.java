61c065141ce8efead4e2c0d3a3a7360868550f87
Alciliano da Silva Lima <alcilianodasilva@gmail.com>
2026-06-27 17:12:08 -0300

    feat: Coloca o LOMBOK na classe para abstração

--- src/main/java/br/com/estudo/cadastrodepessoas/Pessoas/PessoaModel.java
+++ src/main/java/br/com/estudo/cadastrodepessoas/Pessoas/PessoaModel.java
@@ -2,12 +2,18 @@ package br.com.estudo.cadastrodepessoas.Pessoas;
 
 import br.com.estudo.cadastrodepessoas.Tarefas.TarefasModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 // @Entity transforma uma classe em uma entidade do banco de dados
 @Entity
 @Table(name = "tb_pessoas")
+@NoArgsConstructor // LOMBOK cria um construtor vazio
+@AllArgsConstructor // LOMBOK cria um construtor com todos os parâmetros
+@Data // LOMBOK cria os getters and setters
 public class PessoaModel {
 
     @Id
@@ -21,37 +27,5 @@ public class PessoaModel {
     @JoinColumn(name = "tarefas_id")  // chave estrangeira
     private TarefasModel tarefas;
 
-    public PessoaModel() {
 
-    }
-
-    public PessoaModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
--- src/main/java/br/com/estudo/cadastrodepessoas/Tarefas/TarefasModel.java
+++ src/main/java/br/com/estudo/cadastrodepessoas/Tarefas/TarefasModel.java
@@ -2,11 +2,17 @@ package br.com.estudo.cadastrodepessoas.Tarefas;
 
 import br.com.estudo.cadastrodepessoas.Pessoas.PessoaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_tarefas")
+@NoArgsConstructor // LOMBOK cria um construtor vazio
+@AllArgsConstructor // LOMBOK cria um construtor com todos os parâmetros
+@Data // LOMBOK cria os getters and setters
 public class TarefasModel {
 
     @Id
